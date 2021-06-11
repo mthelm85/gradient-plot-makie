2ad9f26ed9e7a9a8929428c4bd79b10a3ca3a391
Matt Helm <30706758+mthelm85@users.noreply.github.com>
2021-06-11 12:54:27 -0500

    Update index.jl

--- index.jl
+++ index.jl
@@ -7,22 +7,22 @@ x = -1:0.1:1.0
 y = -1:0.1:1.0
 z = [f(i,j) for i in x, j in y]
 
-scene = surface(x,y,z)
-xm, ym, zm = minimum(scene_limits(scene))
-contour!(scene, x, y, z, levels = 15, linewidth = 2, transformation = (:xy, zm))
-wireframe!(scene, x, y, z, overdraw = true, transparency = true, color = (:black, 0.1))
-center!(scene) # center the Scene on the display
-
-
 xy = [(i,j) for i in x for j in y]
 # This is the same function as above, just modified so that it will work with ForwardDiff
 g(x,y) = [-5*x*y*exp(-x^2-y^2)]
 # Jacobians are scaled by 0.05 so that the vector arrows aren't too long when plotted
 J = .05 .* [ForwardDiff.jacobian(x -> g(x[1], x[2]), [i[1],i[2]]) for i in xy]
 # All the x coordinates
-xs = [xy[i][1] for i in 1:7:length(xy)]
+xs = [xy[i][1] for i in 1:length(xy)]
 # All the y coordinates
-ys = [xy[i][2] for i in 1:7:length(xy)]
+ys = [xy[i][2] for i in 1:length(xy)]
 # We need u,v for the quiver plot
-u = [J[i][1] for i in 1:7:length(J)]
-v = [J[i][2] for i in 1:7:length(J)]
+u = [J[i][1] for i in 1:length(J)]
+v = [J[i][2] for i in 1:length(J)]
+
+scene = surface(x,y,z)
+xm, ym, zm = minimum(scene_limits(scene))
+contour!(scene, x, y, z, levels = 15, linewidth = 2)
+arrows!(xs,ys,u,v, arrowsize=0.025, linewidth=0.5, transformation = (:xy, zm))
+wireframe!(scene, x, y, z, overdraw = true, transparency = true, color = (:black, 0.1))
+center!(scene) # center the Scene on the display
